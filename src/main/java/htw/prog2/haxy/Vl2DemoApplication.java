54ea5366b3fc1730b70d42ff4a18c9996b09776e
bonguen <burakong44@outlook.de>
2024-10-24 16:47:16 +0200

    initial commit

---
+++ src/main/java/htw/prog2/haxy/Vl2DemoApplication.java
@@ -0,0 +1,13 @@
+package htw.prog2.haxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vl2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vl2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/prog2/haxy/Vl2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.prog2.haxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vl2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
